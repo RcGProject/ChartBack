6862bdbdb50c071173345e48229bd6429d1417dc
Jefferson Alves <jeeffsantoos@outlook.com>
2023-04-02 11:47:50 -0300

    ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,3 @@
-# ChartProjectB
-Repositório dedicado para o projeto QuickChart | BackEnd
-
-
-# copie essas linhas no seu settings.json
-    "editor.codeActionsOnSave": {
-        "source.fixAll.eslint": true
-    }
-
-# Configurações do ESLint
-## Este projeto utiliza o ESLint para garantir a qualidade do código. As seguintes configurações foram aplicadas no arquivo .eslintrc.js:
-
 module.exports = {
   env: {
     browser: true,
@@ -77,5 +65,3 @@ module.exports = {
     ],
   },
 };
-
-As configurações acima definem o ambiente de execução do código (browser: true e es2021: true), a extensão do ESLint (airbnb-base), as opções de parser (ecmaVersion: 'latest' e sourceType: 'module') e as regras de qualidade do código que serão verificadas. Algumas regras foram desabilitadas (no-console, class-methods-use-this, import/first, import/no-extraneous-dependencies, import/prefer-default-export) e outras foram habilitadas (import/no-default-export, import/order) com suas respectivas configurações específicas.
