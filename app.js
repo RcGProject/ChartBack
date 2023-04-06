307f83eaaa7b9d21b6e2b45b42c2c91b723e009b
Jefferson Alves <jeeffsantoos@outlook.com>
2023-04-06 00:08:55 -0300

    MongoDB connect

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// Importa o módulo dotenv para carregar variáveis de ambiente
-import dotenv from 'dotenv';
-dotenv.config();
-
 // Importa o módulo express para criar o servidor
-import express from 'express';
+import cors from "cors";
+import express from "express";
 
+import { connectBD, sessionOptions } from "./src/config/database";
 // Importa as rotas definidas na pasta src/routes
-import homeRoutes from './src/routes/homeRoutes';
+import homeRoutes from "./src/routes/homeRoutes";
 
 // Cria a classe App, que será responsável por configurar o servidor
 class App {
   constructor() {
     this.app = express();
     this.middlewares();
+    this.cors();
     this.routes();
+    this.connect();
+    this.sessionOptionsBD();
   }
 
   // Método responsável por configurar os middlewares do servidor
@@ -24,7 +25,23 @@ class App {
 
   // Método responsável por definir as rotas do servidor
   routes() {
-    this.app.use('/', homeRoutes);
+    this.app.use("/", homeRoutes);
+  }
+
+  cors() {
+    this.app.use(cors());
+  }
+
+  connect() {
+    connectBD
+      .then(() => {
+        this.app.emit("pronto"); // Emite o evento 'pronto' para iniciar o servidor
+      })
+      .catch((e) => console.log(e));
+  }
+
+  sessionOptionsBD() {
+    this.app.use(sessionOptions);
   }
 }
 
